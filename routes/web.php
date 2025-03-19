c32e9d8ce5392715520d6883761b6014ff60116a
davigabrielsp <davigabriel.sp@outlook.com>
2025-03-19 08:36:47 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+   return view('welcome');
+})->name('home');
+
+Route::get('/contact', function(){
+   return redirect()->route('home');
+})->name('contact');
+
+Route::name('admin.')->prefix('admin')->group(function(){
+    Route::get('/', function(){
+        return 'admin home';
+    });
+    Route::get('create', function(){
+        return 'admin create';
+    })->name('create');
 });
